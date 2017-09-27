37fe959c17219daa67dcdefd9df661a3ff51b8a4
MeenaKamran <kamran_austin@yahoo.com>
2017-09-27 12:18:32 -0700

    modified after the udacity review recommendations

--- designs.js
+++ designs.js
@@ -8,29 +8,21 @@ function makeGrid() {
 // Your code goes here!
   const height = $('#input_height').val();
   const width = $('#input_width').val();
-  const pickedColor = document.getElementById('colorPicker').value;
   const grid = document.getElementById('pixel_canvas');
 
   let dynTableHtmlString = "";
   $('#pixel_canvas').empty();
   for (let i=0; i < height; i++) {
-    dynTableHtmlString += '<tr>';
+    const row = grid.insertRow(i);
     for (let j=0; j < width; j++) {
-      dynTableHtmlString += '<td></td>';
-    }
-    dynTableHtmlString += '</tr>';
-  }
-  $('#pixel_canvas').append(dynTableHtmlString);
-
-  const rows = grid.getElementsByTagName('tr');
-  for (let i=0; i<height; i++) {
-    const row = grid.rows[i];
-    for (let j=0; j<width; j++) {
-      row.children[j].addEventListener('click', function() {
+      const cell = row.insertCell(j);
+      cell.addEventListener('click', function() {
+        const pickedColor = document.getElementById('colorPicker').value;
         this.style.backgroundColor = pickedColor;
       });
     }
   }
+  $('#pixel_canvas').append(dynTableHtmlString);
 
 }
 
